20e503363920df6cf5ef637eca7584c8bad11354
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2023-12-10 23:47:31 +0100

    update

--- TravelSnap/src/views/NewPost.vue
+++ TravelSnap/src/views/NewPost.vue
@@ -20,13 +20,16 @@ import {
   IonToolbar,
   toastController, IonItemSliding, IonItemOptions, IonItemOption
 } from '@ionic/vue';
-import { imageOutline, cameraOutline, earthOutline, trash } from 'ionicons/icons';
+import { imageOutline, cameraOutline, earthOutline, trash, heart, chatboxEllipsesOutline } from 'ionicons/icons';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Geolocation } from '@capacitor/geolocation';
 import { useRouter } from 'vue-router';
+import { authService } from '@/service/firebase.authService';
+
 
 const router = useRouter();
-const title = ref('');
+const user = authService.currentUser();
+
 const description = ref('');
 const image = ref<string | null>(null);
 const geopoint = ref<{ lat: number; lng: number } | null>(null);
@@ -130,110 +133,226 @@ const submitPost = async () => {
 
 <template>
   <ion-page>
-    <ion-content class="ion-padding">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Create a post!</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title size="large">Create a post!</ion-title>
+      </ion-toolbar>
+    </ion-header>
 
-      <ion-list>
-        <ion-item>
-          <IonInput v-model="title" aria-label="title" placeholder="Title" color="dark"></IonInput>
-        </ion-item>
-        <ion-item>
-          <IonTextarea v-model="description" aria-label="Description" placeholder="Description"></IonTextarea>
-        </ion-item>
-
-        <ion-item>
-          <IonIcon :icon="imageOutline" @click="selectImage" class="icon-style" aria-label="upload picture">
-          </IonIcon>
-          <IonIcon :icon="cameraOutline" @click="takePicture" class="icon-style" aria-label="upload picture">
-          </IonIcon>
-          <IonIcon :icon="earthOutline" @click="getCurrentLocation" class="icon-style" aria-label="Get location">
-          </IonIcon>
-        </ion-item>
-        <ion-item v-if="image">
+    <ion-content>
+      <hr>
+      <div class="preview-container">
+        <div class="preview">
           <div class="image-container">
-            <IonIcon :icon="trash" @click="removeImage" class="delete-icon"></IonIcon>
             <img :src="image || ''" />
+            <div class="preview-overlay">
+              <div class="overlay-content">
+                <h2 class="overlay-title">Preview Title</h2>
+                <p class="overlay-description">{{ description  }}</p>
+              </div>
+              <div class="overlay-icons">
+                <IonIcon :icon="earthOutline" aria-hidden="true"></IonIcon>
+                <IonIcon :icon="heart" color="danger" aria-hidden="true"></IonIcon>
+                <IonIcon :icon="chatboxEllipsesOutline" aria-hidden="true"></IonIcon>
+              </div>
+            </div>
           </div>
-        </ion-item>
-        <ion-item>
-          <ion-button size="small" @click="submitPost">Submit</ion-button>
-        </ion-item>
-      </ion-list>
+        </div>
+      </div>
+
+      <hr>
+
+      <ion-item class="image-geo-import-bar">
+        <div class="image-geo-import-container">
+          <IonIcon :icon="imageOutline" @click="selectImage" aria-label="Upload Image"></IonIcon>
+          <IonIcon :icon="cameraOutline" @click="takePicture" aria-label="Upload Image"></IonIcon>
+          <IonIcon :icon="earthOutline" @click="getCurrentLocation" aria-label="Get Location"></IonIcon>
+        </div>
+      </ion-item>
+
+      <hr>
+
+      <ion-item class="geopoint-display">
+        <div class="geopoint-info">
+          <div v-if="geopoint && geopoint.lat && geopoint.lng" class="coordinates">
+            <ion-label class="coordinates-label">Post coordinates:</ion-label>
+            <ion-label>Lat: {{ geopoint.lat }}</ion-label>
+            <ion-label>Lng: {{ geopoint.lng }}</ion-label>
+          </div>
+          <div v-else>
+            <ion-label class="coordinates-label">Set location by clicking globe</ion-label>
+          </div>
+        </div>
+      </ion-item>
+
+      <hr>
+
+      <ion-item class="item-input-container">
+          <IonInput v-model="description" placeholder="Description"></IonInput>
+      </ion-item>
+
+      <hr>
+      <hr>
+
+      <ion-item class="item-button-container">
+        <div class="button-container">
+          <ion-button class="submit-button" @click="submitPost">Post</ion-button>
+        </div>
+      </ion-item>
+
     </ion-content>
   </ion-page>
 </template>
 
   
 <style scoped>
-ion-button {
-  --background: var(--ion-color-success);
-  padding-top: 0;
-  margin: 0;
-}
-
-ion-item {
-  margin: 0;
-  width: 100%;
-}
-
-ion-list {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-}
-
-ion-page,
+/* CSS for background and title */
 ion-content {
-  --ion-background-color: #202020;
-  color: #FFFFFF;
-  width: 100%;
-  margin: 0;
-  padding: 0;
+  --background: #202020;
 }
 
-ion-item,
-ion-label,
-ion-button,
-ion-input,
-ion-textarea,
+ion-toolbar {
+  --background: #202020;
+  --color: var(--ion-color-success);
+}
+
 ion-title {
-  --color: #ffffff;
+  text-align: center;
 }
 
-.icon-style {
-  font-size: 24px;
-  color: var(--ion-color-primary);
-  margin: 5px;
+/* CSS for preview */
+.preview-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 45vh;
+}
+
+.preview {
+  width: 80%;
+  height: 100%;
+  position: relative;
 }
 
 .image-container {
-  position: relative;
-  display: inline-block;
+  width: 100%;
+  height: 100%;
 }
 
 .image-container img {
   width: 100%;
-  height: auto;
-  margin: 0;
-  padding: 0;
+  height: 100%;
+  object-fit: fit-content;
+  border-radius: 10px;
 }
 
-.delete-icon {
+.preview-overlay {
   position: absolute;
-  top: 0;
+  bottom: 0;
+  left: 0;
   right: 0;
-  color: var(--ion-color-danger);
-  font-size: 30px;
-  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  color: #fff;
 }
 
-.confirm-location-button {
-  --background: var(--ion-color-primary);
-  margin-top: 10px;
+.overlay-content {
+  text-align: left;
+}
+
+.overlay-icons {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+
+.overlay-icons ion-icon {
+  font-size: 24px;
+  margin-bottom: 50%;
+}
+
+.overlay-title {
+  font-size: 1.5em;
+}
+
+.overlay-description {
+  font-size: 1em;
+}
+
+/* CSS for image-import and geolocation-import bar */
+ion-item.image-geo-import-bar {
+  --background: #202020;
+  --border-color: transparent;
+  border-top: 1px solid #fff;
+  border-radius: 10px;
+}
+
+.image-geo-import-container {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+}
+
+.image-geo-import-container ion-icon {
+  font-size: 30px;
+  color: var(--ion-color-success);
+}
+
+ion-item.geopoint-container {
+  --color: black;
+
+}
+
+/* Geolocation Text */
+ion-item.geopoint-display {
+  --background: #202020;
+  --border-color: #fff;
+  display: flex;
+  justify-content: center;
+}
+
+.geopoint-info {
+  text-align: start;
+}
+
+.coordinates-label {
+  font-size: 1.2em;
+  color: #fff;
+}
+
+.coordinates ion-label {
+  display: block;
+  color: #a56a3a;
+}
+
+/* Description input field */
+
+ion-item.item-input-container {
+  --background: #202020;
+  --border-color: #fff;
+  display: flex;
+  justify-content: center;
+}
+
+.item-input-container ion-input {
+  --color: #fff;
+}
+
+ion-item.item-button-container {
+  --background: #202020;
+  --border-color: transparent;
+}
+
+.button-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+ion-button.submit-button {
+  --background: var(--ion-color-success);
+  font-size: large;
 }
 </style>
   
